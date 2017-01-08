669ce22b5d27aa8fcb60d7c4ea45d5695d3df845
erikzaadi <erik.zaadi@gmail.com>
2017-01-08 08:09:23 +0200

    Follow Github redirects

--- travis-install.sh
+++ travis-install.sh
@@ -8,7 +8,6 @@ go get -v github.com/tdewolff/minify/cmd/minify
 
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e $TRAVIS_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  curl -s https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xvzf -
+  curl -s -L https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xvzf -
   mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $TRAVIS_BUILD_DIR/bin/hugo
 fi
-
