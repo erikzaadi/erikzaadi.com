68217037d57ae2c3d871ee7f7898d533d5e713eb
erikzaadi <erik.zaadi@gmail.com>
2017-01-08 09:42:35 +0200

    Fix NMA

--- notify.sh
+++ notify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST https://www.notifymyandroid.com/publicapi/notify -d "{\"apikey\": \"${NMA_TOKEN}\", \"application\": \"Travis\", \"event\": \"Deploy of blog succeeded\!\" }"
+curl -X POST https://www.notifymyandroid.com/publicapi/notify -d "apikey=${NMA_TOKEN}&application=Travis&event=Deploy of blog succeeded&description=SUCCESS&url=http://erikzaadi.com"
