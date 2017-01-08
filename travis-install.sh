df43393779830c393f215f31a5cdd59849048510
erikzaadi <erik.zaadi@gmail.com>
2017-01-08 08:11:19 +0200

    GoPATH instead of Travis

--- travis-install.sh
+++ travis-install.sh
@@ -7,7 +7,7 @@ mkdir -p "$TRAVIS_BUILD_DIR/gosrc"
 go get -v github.com/tdewolff/minify/cmd/minify
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $TRAVIS_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+if [ ! -e ${TRAVIS_BUILD_DIR}/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   curl -s -L https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xvzf -
-  mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $TRAVIS_BUILD_DIR/bin/hugo
+  mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ${GOPATH}/bin/hugo
 fi
