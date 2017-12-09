a31f79b918d0016d72b1bb24a0ae1f733cbb99e2
erikzaadi <erik.zaadi@gmail.com>
2017-12-09 22:16:26 +0200

    StupidErikException

--- travis-install.sh
+++ travis-install.sh
@@ -9,6 +9,5 @@ go get -v github.com/tdewolff/minify/cmd/minify
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e ${TRAVIS_BUILD_DIR}/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   curl -s -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz | tar xvzf -
-  https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Linux-64bit.tar.gz
   mv ./hugo ${GOPATH}/bin/hugo
 fi
